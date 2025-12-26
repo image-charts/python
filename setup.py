d6572da052bfe5ef89370a00f0b4c3fd2f1ebb81
Francois-Guillaume Ribreau <github@fgribreau.com>
2025-12-26 23:20:38 +0000

    ⚡ update

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r") as fh:
 
 setup(
   name='image-charts',
-  version="6.1.31",
+  version="6.1.32",
   py_modules=['ImageCharts'],
   url='https://github.com/image-charts/python',
   license='MIT',
